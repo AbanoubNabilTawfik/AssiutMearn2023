7a2c9df7e5cf96d27edb7a09ab2f94c04a3b6375
Abanoub Nabil <abanoub.nabil2016@gmail.com>
2023-05-22 11:57:36 +0300

    day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { LeftNavComponent } from './left-nav/left-nav.component';
+import { RightNavComponent } from './right-nav/right-nav.component';
+import { InterpolationComponent } from './interpolation/interpolation.component';
+import { PropertyBindingComponent } from './property-binding/property-binding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    LeftNavComponent,
+    RightNavComponent,
+    InterpolationComponent,
+    PropertyBindingComponent
   ],
   imports: [
     BrowserModule,
